d19ab2b8d8d4bd47b90f26f1beae459abccad4a7
Liu Qun <liuqun@aparadigm.onaliyun.com>
2019-07-01 06:28:28 -0700

    开始加入DTLS相关代码到dlts_echo_server中
    Signed-off-by: Liu Qun <liuqun@aparadigm.onaliyun.com>

--- tests/ASIO-UDP-test/dtls_echo_server.cpp
+++ tests/ASIO-UDP-test/dtls_echo_server.cpp
@@ -25,15 +25,73 @@ const int DEFAULT_PORT = 8007; // default Unix echo service port 7 requires root
 
 #include <openssl/ssl.h>
 
-extern "C"{
 
-typedef struct _peer_info_t {
-    SSL *ssl;
-    BIO *rbio;
-    BIO *wbio;
-}peer_info_t;
 
+
+class PeerRecord {
+public:
+
+
+
+public:
+    PeerRecord(const udp::endpoint& peer_ip_and_port): m_peer_ip_and_port(peer_ip_and_port)
+    {
+        m_ssl = nullptr;
+        m_incoming = m_outgoing = nullptr;
+    }
+    //explicit PeerRecord(string name, string phone, string address): name(name), phone(phone), address(address) {}
+
+    // // 重写自定义的"等于等于"运算符 @Override operator==
+    // bool operator==(const PeerRecord& other)
+    // {
+    //     return this->m_peer_ip_and_port == other.m_peer_ip_and_port;
+    // }
+
+
+public:
+    SSL *m_ssl;
+    BIO *m_incoming; // incoming DTLS ciphertext data will be cached in this BIO
+    BIO *m_outgoing; // outgoing DTLS ciphertext data will be cached in this BIO
+    //std::string debug;
+    udp::endpoint m_peer_ip_and_port;
 };
+
+// bool operator==(const PeerRecord& lhs, const PeerRecord& rhs) {
+//     return lhs.m_peer_ip_and_port == rhs.m_peer_ip_and_port;
+// }
+
+
+// 自定义的 C++ 哈希计算模板 hash<udp::endpoint>
+// 并且将此模板注入进 namespace std 默认命名空间
+namespace std
+{
+    template <> struct hash<udp::endpoint>
+    {
+        std::size_t operator()(const udp::endpoint& peer_ip_and_port) const
+        {
+            const std::string& ip_address_str = peer_ip_and_port.address().to_string();
+            int port = peer_ip_and_port.port();
+
+            std::hash<std::string> digest_from_string;
+            std::hash<int> digest_from_int;
+            return digest_from_string(ip_address_str) ^ digest_from_int(port);
+        }
+    };
+}
+
+/*
+struct MyHashAlgorithm{
+    std::size_t operator()(const udp::endpoint& peer_ip_and_port) const
+    {
+        const std::string& ip_address_str = peer_ip_and_port.address().to_string();
+        int port = peer_ip_and_port.port();
+
+        std::hash<std::string> digest_from_string;
+        std::hash<int> digest_from_int;
+        return digest_from_string(ip_address_str) ^ digest_from_int(port);
+    }
+};
+*/
 
 int main()
 {
@@ -43,8 +101,8 @@ int main()
     udp::endpoint client_ip_and_port;
     asio::error_code err_code_bind;
     asio::error_code err_code_send;
-    boost::array<char, 1500> plaintext;
-    std::unordered_map<int, peer_info_t> peer_tbl;
+    boost::array<char, 1500> recvbuf;
+    std::unordered_map<udp::endpoint, PeerRecord> connected_peers;
 
     socket.bind(server_ip_and_port, err_code_bind);
     if (err_code_bind) {
@@ -55,19 +113,26 @@ int main()
         return 0;
     }
     for (;;) {
-        int n = socket.receive_from(asio::buffer(plaintext), client_ip_and_port);
-        fprintf(stdout, "DEBUG: server received %d bytes, plaintext[0] = 0x%02X\n", n, plaintext[0]);
-        std::unordered_map<int, peer_info_t>::iterator peer;
-        const int port = client_ip_and_port.port();
-        peer = peer_tbl.find(port);
-        if (peer_tbl.end() == peer) {
-            peer_info_t info;
-            info.ssl = NULL;
-            info.rbio = info.wbio = NULL;
-            peer_tbl[port] = info;
+        int n = socket.receive_from(asio::buffer(recvbuf), client_ip_and_port);
+        fprintf(stdout, "DEBUG: server received %d bytes, recvbuf[0] = 0x%02X\n", n, recvbuf[0]);
+        PeerRecord *ptr=nullptr;
+        auto got = connected_peers.find(client_ip_and_port); // std::unordered_map<udp::endpoint,PeerRecord>::const_iterator
+        if (connected_peers.end() == got) /* 若查不到来自此客户端的访问记录 */ {
+            ptr = new PeerRecord(client_ip_and_port);
+            PeerRecord& record = *ptr;
+            record.m_ssl = nullptr;
+            record.m_incoming = nullptr;
+            record.m_outgoing = nullptr;
+            auto ret = connected_peers.insert(std::pair<udp::endpoint, PeerRecord> {client_ip_and_port, record});
         }
-        fprintf(stdout, "DEBUG: peer_tbl.size() = %d\n", (int)peer_tbl.size());
-        std::string reply(plaintext.data(), n);
+        else
+        {
+            ptr = &(got->second);
+        }
+        //BIO_write(record.incoming
+
+        fprintf(stdout, "DEBUG: connected_peers.size() = %d\n", (int)connected_peers.size());
+        std::string reply("a", 1);
         socket.send_to(asio::buffer(reply), client_ip_and_port, 0, err_code_send);
     }
     return 0;
