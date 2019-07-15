5cbcc400df0421eb9356b1e81c4894102e7a3d70
刘群 <qun.liu@sdxformula.com>
2019-07-15 19:19:43 +0800

    剔除自定义BIO中无用的dead code代码
    - 删除 BIO_s_custom_write_ex()
    - 删除 BIO_s_custom_read_ex()
    - 让 BIO_s_custom_read() 始终返回 -1 表示无法读入

--- tests/ASIO-UDP-test/dtls_echo_server.cpp
+++ tests/ASIO-UDP-test/dtls_echo_server.cpp
@@ -445,14 +445,7 @@ int server_app_verify_cookie(SSL *ssl, const unsigned char *cookie, unsigned int
 
 
 
-static
-int BIO_s_custom_write_ex(BIO *b, const char *data, size_t dlen, size_t *written)
-{
-    //fprintf(stderr, "BIO_s_custom_write_ex(BIO[0x%016lX], data[0x%016lX], dlen[%ld], *written[%ld])\n", b, data, dlen, *written);
-    fflush(stderr);
 
-    return -1;
-}
 
 static
 int BIO_s_custom_write(BIO *b, const char *data, int dlen)
@@ -486,50 +479,11 @@ fprintf(stderr, "DEBUG--!!!!!!! 准备发包给 client IP=%s, port=%d\n", ip, po
     return dlen;
 }
 
-static
-int BIO_s_custom_read_ex(BIO *b, char *data, size_t dlen, size_t *readbytes)
-{
-    //fprintf(stderr, "BIO_s_custom_read_ex(BIO[0x%016lX], data[0x%016lX], dlen[%ld], *readbytes[%ld])\n", b, data, dlen, *readbytes);
-    //fflush(stderr);
-
-    return -1;
-}
 
 static
 int BIO_s_custom_read(BIO *b, char *data, int dlen)
 {
-    return 1;
-    // int ret;
-    // custom_bio_data_t *cdp;
-    // deque_t *dp;
-    // buffer_t *bp;
-
-    // ret = -1;
-    // fprintf(stderr, "BIO_s_custom_read(BIO[0x%016lX], data[0x%016lX], dlen[%ld])\n", b, data, dlen);
-    // fprintf(stderr, "  probe peekmode %d\n",
-    //         ((custom_bio_data_t *)BIO_get_data(b))->peekmode);
-    // fflush(stderr);
-
-    // cdp = (custom_bio_data_t *)BIO_get_data(b);
-    // dp = &cdp->rxqueue;
-    // fprintf(stderr, "  data[0x%016lX] queue: %d\n", dp, deque_count(dp));
-    // if (dp->head)
-    // {
-    //     if (((custom_bio_data_t *)BIO_get_data(b))->peekmode)
-    //         bp = (buffer_t *)deque_peekleft(dp);
-    //     else
-    //         bp = (buffer_t *)deque_popleft(dp);
-    //     fprintf(stderr, "  buf[0x%016lX]\n", bp);
-    //     fflush(stderr);
-
-    //     ret = (bp->len<=dlen) ? bp->len : dlen;
-    //     memmove(data, bp->buf, ret);
-
-    //     if (!((custom_bio_data_t *)BIO_get_data(b))->peekmode)
-    //         buffer_free(bp);
-    // }
-
-    // return ret;
+    return -1;
 }
 
 //int BIO_s_custom_gets(BIO *b, char *data, int size);
